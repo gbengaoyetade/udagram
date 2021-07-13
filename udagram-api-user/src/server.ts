5c99eb2e96e3789e1d76b5df3d1cef40f119a368
gbengaoyetade <ioyetade@gmail.com>
2021-07-13 06:04:33 +0100

    modify api-user

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -5,10 +5,9 @@ import { sequelize } from "./sequelize";
 import { IndexRouter } from "./controllers/v0/index.router";
 
 import { config } from "./config/config";
-import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
+import { V0_USER_MODELS } from "./controllers/v0/model.index";
 
 (async () => {
-  await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
 
